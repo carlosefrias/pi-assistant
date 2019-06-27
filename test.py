f97a71db92b697454419a3e6957e826f0673a3b5
carlosefrias <carlosefrias@gmail.com>
2019-06-27 18:47:08 +0100

    test script updated

--- test.py
+++ test.py
@@ -1,3 +1,20 @@
 import browser_laucher as bl 
+import sys
 
-bl.open_url('https://v1.mrpiracy.xyz/')
+piracy = 'https://v1.mrpiracy.xyz/'
+youtube = 'www.youtube.com'
+sports = 'idimsports.eu'
+google = 'www.google.pt'
+
+site = google
+tema = sys.argv[1]
+
+if (tema == 'filmes'):
+    site = piracy
+elif (tema == 'futebol'):
+    site = sports
+elif (tema == 'youtube'):
+    site = youtube
+
+print("A abrir site: " + site)
+bl.open_url(site)
